b3012a52e70c71a29e086fc92a6723e47429856b
Koldo Abasolo Iturregi <kabasolo@c3r6s3.42urduliz.com>
2024-01-31 10:18:44 +0100

    BuenoBonitoBarato

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: kabasolo <kabasolo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/13 11:25:05 by kabasolo          #+#    #+#             */
-/*   Updated: 2024/01/31 10:18:03 by kabasolo         ###   ########.fr       */
+/*   Updated: 2024/01/31 10:18:40 by kabasolo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,8 +34,8 @@ void	*ft_memcpy(void *dst, const void *src, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t len);
 size_t	ft_strlcpy(char *dst, const char *src, size_t size);
 size_t	ft_strlcat(char *dst, const char *src, size_t size);
-int		ft_toupper(int c);
-int		ft_tolower(int c);
+int	ft_toupper(int c);
+int	ft_tolower(int c);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
 int	ft_strncmp(const char *s1, const char *s2, size_t n);
